53db777533540ad013c84bfaaf976c6b8b3b7c07
unknown <eissa.hussein95@gmail.com>
2025-10-08 04:28:57 +0300

    [dyad] Moved TooltipProvider to wrap the entire layout - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,24 +6,23 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
-// LayoutProps no longer needs 'children' directly as Outlet handles nested content
 interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow container py-8">
-        <TooltipProvider>
-          <div> {/* Changed from React.Fragment to a div */}
+    <TooltipProvider> {/* Moved TooltipProvider to wrap the entire layout div */}
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow container py-8">
+          <div>
             <Outlet />
           </div>
-        </TooltipProvider>
-      </main>
-      <Footer />
-      <Toaster />
-      <Sonner />
-    </div>
+        </main>
+        <Footer />
+        <Toaster />
+        <Sonner />
+      </div>
+    </TooltipProvider>
   );
 };
 
